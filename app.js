1976389339fd2539ffaaf6556a01399b3b757929
domespa <dumiii1988@gmail.com>
2025-01-20 21:36:05 +0100

    feat: add rotta bacheca

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ app.get('/', (req, res) => {
   res.send('Server del mio Blog')
 })
 
-
+app.get("/bacheca", (req, res) => {
+    res.json([])
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
