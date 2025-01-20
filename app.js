ef49c9b40781b1fbda3719d05e8d211ab3ba7dee
domespa <dumiii1988@gmail.com>
2025-01-20 21:33:20 +0100

    feat: add rotta Server del mio blog

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Server del mio Blog')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
